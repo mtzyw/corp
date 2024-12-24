6c65b7e428ce6ce19627521fbf8d90b30f906e7b
mtzyw <92630104+mtzyw@users.noreply.github.com>
2024-12-24 20:35:04 +0800

    Update page.tsx

--- src/app/(hero)/Performance/page.tsx
+++ src/app/(hero)/Performance/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '@/components/hero'
-import performanceSrc from '/public/Performance.jpg'    
+import performanceSrc from '/public/performance.jpg'    
 
 import { Metadata } from 'next'
 
