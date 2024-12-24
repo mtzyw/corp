3a7921b61be499748355e32befbae101485e5dc9
mtzyw <92630104+mtzyw@users.noreply.github.com>
2024-12-24 20:35:23 +0800

    Update page.tsx

--- src/app/(hero)/Reliability/page.tsx
+++ src/app/(hero)/Reliability/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '@/components/hero'
-import reliabilitySrc from '/public/Reliability.jpg'    
+import reliabilitySrc from '/public/reliability.jpg'    
 import { Metadata } from 'next'
 
 export const metadata:Metadata = {
